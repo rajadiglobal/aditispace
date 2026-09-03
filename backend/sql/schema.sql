8f2814e215d1d5e16574b186f530dc7012b814dc
siddhartha mukherjee <siddmuk2005@gmail.com>
2026-09-03 21:49:10 +0530

    updated database

--- backend/sql/schema.sql
+++ backend/sql/schema.sql
@@ -1,4 +1,7 @@
-﻿CREATE TABLE alembic_version (
+﻿
+CREATE DATABASE IF NOT EXISTS aditi_store;
+use aditi_store;
+CREATE TABLE alembic_version (
     version_num VARCHAR(32) NOT NULL, 
     CONSTRAINT alembic_version_pkc PRIMARY KEY (version_num)
 );
